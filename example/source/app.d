b68588ff77e06ff7c8e384d320052b23e65a91c0
Azbuka_slovensko <azbuka.slovensko@gmail.com>
2016-03-27 17:55:49 +0200

    Update of example program

--- example/source/app.d
+++ example/source/app.d
@@ -2,8 +2,10 @@ import std.stdio;
 import BrightProof;
 
 int main(string[] args) {
-	if(args.length < 2)
+	if(args.length < 2) {
+		usage(args);
 		return -1;
+	}
 	SemVer a,b;
 	try {
 		a = SemVer(args[1]);
@@ -14,8 +16,26 @@ int main(string[] args) {
 		return -2;
 	}
 
-	writeln("Equal?: ", a == b);
-	writeln("Greater?: ", a > b);
-	writeln("Less?: ", a < b);
+	string symbol;
+	if(a > b)
+		symbol = ">";
+	else if(a < b)
+		symbol = "<";
+	else if(a == b)
+		symbol = "=";
+	else
+		symbol = "O RLY?";
+
+	writefln("%s %s %s",
+		a.toString,
+		symbol,
+		b.toString);
 	return 0;
+}
+
+void usage(string[] args) {
+	writefln("Usage:\n" ~
+	 "%s <ver1> <ver2>\n" ~
+	 "Program just compares versions",
+	args[0]);
 }
